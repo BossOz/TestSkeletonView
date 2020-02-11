741d9e843d01d223d5925b692ea1125502145a7d
Oscar Manzelli <omanzelli@gmail.com>
2020-02-11 19:59:07 +0100

    Initial Commit

---
+++ TestSkeletonView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSkeletonView
+//
+//  Created by Oscar on 11/02/2020.
+//  Copyright © 2020 Oscar Manzelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
